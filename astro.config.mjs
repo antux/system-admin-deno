230687c249d0de9e1d343d9315c34c6b55ca1581
Antony Petrocelli <petrocellia@gmail.com>
2025-06-20 02:42:22 -0400

    elimino el soporte de pwa

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import AstroPWA from "@vite-pwa/astro";
+// import AstroPWA from "@vite-pwa/astro";
 import deno from "@deno/astro-adapter";
 
 // https://astro.build/config
@@ -11,28 +11,28 @@ export default defineConfig({
     tailwind({
       applyBaseStyles: false,
     }),
-    AstroPWA({
-      registerType: "autoUpdate", // Auto-actualización
-      includeAssets: ["favicon.ico", "robots.txt", "apple-touch-icon.png"],
-      manifest: {
-        name: "Mi App Astro PWA",
-        short_name: "AstroPWA",
-        description: "Una PWA hecha con Astro",
-        theme_color: "#ffffff",
-        icons: [
-          {
-            src: "/pwa-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "/pwa-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any maskable",
-          },
-        ],
-      },
-    }),
+    // AstroPWA({
+    //   registerType: "autoUpdate", // Auto-actualización
+    //   includeAssets: ["favicon.ico", "robots.txt", "apple-touch-icon.png"],
+    //   manifest: {
+    //     name: "Mi App Astro PWA",
+    //     short_name: "AstroPWA",
+    //     description: "Una PWA hecha con Astro",
+    //     theme_color: "#ffffff",
+    //     icons: [
+    //       {
+    //         src: "/pwa-192x192.png",
+    //         sizes: "192x192",
+    //         type: "image/png",
+    //       },
+    //       {
+    //         src: "/pwa-512x512.png",
+    //         sizes: "512x512",
+    //         type: "image/png",
+    //         purpose: "any maskable",
+    //       },
+    //     ],
+    //   },
+    // }),
   ],
 });
